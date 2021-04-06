ac7242da6fb1f7ed5866b4edd52bf453021c4852
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2021-04-06 19:30:53 -0400

    ranked the top 50 most similar dog pairs based on the Eucledian distance between their components after being averaged across as samples

---
+++ breeds_ranking.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.linalg import norm
+from pprint import pprint
+from itertools import combinations 
+
+data = np.load("pca_sample_breed.npy", allow_pickle=True)
+
+ob = {}
+for row in data:
+	breed_name = row[len(row) - 1]
+	components = row[0:len(row) -2]
+	try:
+		ob[breed_name][len(ob[breed_name])] = components
+	except Exception as e:
+		ob[breed_name] = {}
+		ob[breed_name][len(ob[breed_name])] = components
+
+new_ob = {}
+for key in ob:
+	if len(ob[key]) > 1:
+		#get all samples
+		all_samples = [np.array(x).astype(float) for x in ob[key].values()]
+		#average the samples to get a single array of components
+		averaged_sample = np.mean(all_samples, axis=0)
+		new_ob[key] = averaged_sample
+
+pairs = {}
+combos = combinations(new_ob.keys(), 2)
+for combo in combos:
+	pair_name = combo[0].replace(" ", "-") + "_vs_" + combo[1].replace(" ", "-")
+	first_el = np.array(new_ob[combo[0]])
+	second_el = np.array(new_ob[combo[1]])
+
+	eucledian_distance = norm(first_el - second_el)
+	pairs[pair_name] = eucledian_distance
+
+sorted_pairs = sorted(pairs, key=pairs.get)
+for sorted_pair in sorted_pairs[0:50]:
+	print(sorted_pair, pairs[sorted_pair])
